a466d24840f38828b291ee9c0da42e5368bc29c1
Lucas Legrand <legrand.lucas0@gmail.com>
2020-11-24 11:33:30 +0100

    wip schema pizza - topping

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -1,16 +1,16 @@
-import { MdLocalPizza as icon } from 'react-icons/md'
+import { MdLocalPizza as icon } from 'react-icons/md';
 
 export default {
   name: 'pizza',
   title: 'Pizzas',
   type: 'document',
-  icon: icon,
+  icon,
   fields: [
     {
       name: 'name',
-      title: 'pizza Name',
+      title: 'Pizza Name',
       type: 'string',
-      description: 'Name of the pizza'
+      description: 'Name of the pizza',
     },
     {
       name: 'slug',
@@ -26,7 +26,7 @@ export default {
       title: 'Image',
       type: 'image',
       optios: {
-        hotsport: true,
+        hotspot: true,
       },
     },
     {
@@ -34,7 +34,32 @@ export default {
       title: 'Price',
       type: 'number',
       description: 'Price of the pizza in cents',
-      validation: Rule => Rule.min(1000).max(50000),
+      validation: (Rule) => Rule.min(1000),
     },
-  ]
-}
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
+    },
+  ],
+
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      const tops = Object.values(toppings).filter(Boolean);
+      return {
+        title,
+        media,
+        subtitle: tops.join(', '),
+      };
+    },
+  },
+};
