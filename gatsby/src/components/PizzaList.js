b2d95a89ac7a4183cb56419573be054a230492d7
Lucas Legrand <legrand.lucas0@gmail.com>
2020-11-25 15:01:15 +0100

    pizza grid

--- gatsby/src/components/PizzaList.js
+++ gatsby/src/components/PizzaList.js
@@ -1,27 +1,50 @@
 import React from 'react';
 import { Link } from 'gatsby'
 import Img from 'gatsby-image';
+import styled from 'styled-components';
+
+
+const PizzaGridStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 4rem;
+  grid-auto-rows: auto auto 500px;
+`;
+
+const PizzaStyles = styled.div`
+  display: grid;
+  /* take your row sizing from the PizzaGridStyles  */
+  @supports not (grid-template-rows: subgrid) {
+    --rows: auto auto 1fr
+  }
+  grid-template-rows: var(--rows, subgrid);
+  grid-row: span 3;
+  grid-gap: 1rem;
+  h2, p {
+    margin: 0;
+  }
+`;
 
 function SinglePizza({ pizza }) {
-return (
-  <div>
-    <Link to={`/pizza/${pizza.slug.current}`}>
-      <h2>
-        <span className="mark">{pizza.name}</span>
-      </h2>
-      <p>{pizza.toppings.map(topping => topping.name).join(', ')}</p>
-      <Img fluid={pizza.image.asset.fluid} alt={pizza.name}></Img>
-    </Link>
-  </div>
-  );
+  return (
+    <PizzaStyles>
+      <Link to={`/pizza/${pizza.slug.current}`}>
+        <h2>
+          <span className="mark">{pizza.name}</span>
+        </h2>
+      </Link>
+        <p>{pizza.toppings.map(topping => topping.name).join(', ')}</p>
+        <Img fluid={pizza.image.asset.fluid} alt={pizza.name}></Img>
+    </PizzaStyles>
+    );
 }
 
 export default function PizzaList({ pizzas }) {
   return(
-    <div>
+    <PizzaGridStyles>
       {pizzas.map(pizza => (
         <SinglePizza key={pizza.id} pizza={pizza}/>
       ))};
-    </div>
+    </PizzaGridStyles>
   );
 }
