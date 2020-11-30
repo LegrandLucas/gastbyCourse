accb5835af3f8eaabfa1001f5d076a41d62c5ceb
Lucas Legrand <legrand.lucas0@gmail.com>
2020-11-30 15:17:35 +0100

    server less functions netlify

---
+++ gatsby/functions/placeOrder/placeOrder.js
@@ -0,0 +1,32 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: 587,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
+
+// test send email
+transporter.sendMail({
+  from: 'Slick Slices <slick@example.com>',
+  to: 'orders@example.com',
+  subject: 'New Order!',
+  hytml: '<p>Your nex pizza order is here!</p>',
+});
+
+exports.handler = async (event, context) => {
+  const info = await transporter.sendMail({
+    from: 'Slick Slices <slick@example.com>',
+    to: 'orders@example.com',
+    subject: 'New Order!',
+    hytml: '<p>Your nex pizza order is here!</p>',
+  });
+  console.log(info);
+  return {
+    statusCode: 200,
+    body: info,
+  };
+};
