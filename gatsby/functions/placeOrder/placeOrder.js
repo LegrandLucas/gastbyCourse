dd49bcdfcdcbcff8204e3e8025ca6b1014a5fe66
Lucas Legrand <legrand.lucas0@gmail.com>
2020-11-30 17:09:55 +0100

    serverless email

--- gatsby/functions/placeOrder/placeOrder.js
+++ gatsby/functions/placeOrder/placeOrder.js
@@ -1,5 +1,29 @@
 const nodemailer = require('nodemailer');
 
+function generateOrderEmail({ order, total }) {
+  return `<div>
+    <h2>Your Recent Order for ${total}</h2>
+    <p>Please start walking over, we will have your order ready in the next 20 mins.</p>
+    <ul>
+      ${order
+    .map(
+      (item) => `<li>
+        <img src="${item.thumbnail}" alt="${item.name}"/>
+        ${item.size} ${item.name} - ${item.price}
+      </li>`,
+    )
+    .join('')}
+    </ul>
+    <p>Your total is <strong>$${total}</strong> due at pickup</p>
+    <style>
+        ul {
+          list-style: none;
+        }
+    </style>
+  </div>`;
+}
+
+// create a transport for nodemailer
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: 587,
@@ -9,24 +33,33 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-// test send email
-transporter.sendMail({
-  from: 'Slick Slices <slick@example.com>',
-  to: 'orders@example.com',
-  subject: 'New Order!',
-  hytml: '<p>Your nex pizza order is here!</p>',
-});
-
 exports.handler = async (event, context) => {
+  const body = JSON.parse(event.body);
+  console.log(body);
+  // Validate the data coming in is correct
+  const requiredFields = ['email', 'name', 'order'];
+
+  for (const field of requiredFields) {
+    console.log(`Checking that ${field} is good`);
+    if (!body[field]) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: `Oops! You are missing the ${field} field`,
+        }),
+      };
+    }
+  }
+
+  // send the email
   const info = await transporter.sendMail({
-    from: 'Slick Slices <slick@example.com>',
-    to: 'orders@example.com',
-    subject: 'New Order!',
-    hytml: '<p>Your nex pizza order is here!</p>',
+    from: "Slick's Slices <slick@example.com>",
+    to: `${body.name} <${body.email}>, orders@example.com`,
+    subject: 'New order!',
+    html: generateOrderEmail({ order: body.order, total: body.total }),
   });
-  console.log(info);
   return {
     statusCode: 200,
-    body: JSON.stringify(info),
+    body: JSON.stringify({ message: 'Success' }),
   };
 };
