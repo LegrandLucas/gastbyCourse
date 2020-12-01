eb5fec468cf1f60c32a38beb729f2e0a9ac60adf
Lucas Legrand <legrand.lucas0@gmail.com>
2020-12-01 16:30:16 +0100

    loading grid

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import useLatestData from '../utils/useLatestData';
+import { HomePageGrid } from '../styles/Grids';
+import LoadingGrid from '../components/LoadingGrid';
 
-function CurrentlySlicing() {
+function CurrentlySlicing({ slicemasters }) {
+  console.log(slicemasters);
   return (
     <div>
-      <p>CurrentlySlicing</p>
+      {!slicemasters && <LoadingGrid count={4} />}
+      {slicemasters && !slicemasters?.length && (
+        <p>No one is working right now!</p>
+      )}
     </div>
   );
 }
-function HotSlices() {
+function HotSlices({ hotSlices }) {
   return (
     <div>
-      <p>HotSlices</p>
+      {!hotSlices && <LoadingGrid count={4} />}
+      {hotSlices && !hotSlices?.length && <p>Nothin' in the Case</p>}
     </div>
   );
 }
@@ -23,10 +30,10 @@ export default function HomePage() {
     <div className="center">
       <h1>The Best Pizza Downtown!</h1>
       <p>Open 11am to 11pm Every Single Day</p>
-      <div>
+      <HomePageGrid>
         <CurrentlySlicing slicemasters={slicemasters} />
         <HotSlices hotSlices={hotSlices} />
-      </div>
+      </HomePageGrid>
     </div>
   );
 }
